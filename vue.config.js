24b711f42a0c83e3ffb1a9b58e153a31dbb06526
Scrooge <yu844856744@qq.com>
2022-11-28 14:23:10 +0800

    修改路由模式

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === "development" ? "/" : "./",
-  outputDir: "dist",
-  assetsDir: "static",
-  productionSourceMap: false,
   devServer: {
+    disableHostCheck: true,
     host: "0.0.0.0",
     port: 4040,
   },
