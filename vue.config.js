57890c2dc3ff31834b2274f9acae7b130836671a
Scrooge <yu844856744@qq.com>
2022-11-28 13:47:54 +0800

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  publicPath: "./",
+  outputDir: "dist",
+  assetsDir: "static",
+  productionSourceMap: false,
   transpileDependencies: true,
 });
